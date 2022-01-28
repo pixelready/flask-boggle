5110154473d5c1b239bda81e7c04b73da8072d69
pixelready <bizartist@gmail.com>
2022-01-28 14:42:14 -0800

    new game API test WIP

--- app.py
+++ app.py
@@ -19,11 +19,28 @@ def homepage():
 
 @app.post("/api/new-game")
 def new_game():
-    """Start a new game and return JSON: {game_id, board}."""
+    """Start a new game and return JSON: {game_id, board}.
+    >>> new_game()
+    >>> json_as_text = json_response.get_data(as_text=True)
+    >>> len(json_as_text.board) == 5
+    True
+    >>> len(json_as_text.board[0]) == 5
+    True
+    >>> type(json_as_text.gameId) == 'str'
+    True
+
+    """
 
     # get a unique string id for the board we're creating
     game_id = str(uuid4())
     game = BoggleGame()
     games[game_id] = game
 
-    return {"gameId": "need-real-id", "board": "need-real-board"}
+    json_response = jsonify({
+        "gameId": game_id,
+        "board": game.board
+    })
+
+    print(f"{json_response.get_data(as_text=True)}")
+
+    return json_response
