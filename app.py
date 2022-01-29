878e270c7fea32fb577dd49d00a95990930070c5
= <stevenmtang1@gmail.com>
2022-01-28 17:03:57 -0800

    WIP: score_word, added logic to check if word is valid and return JSON result

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, jsonify
 from uuid import uuid4
 
 from boggle import BoggleGame
+from wordlist import WordList, english_words
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "this-is-secret"
@@ -35,6 +36,33 @@ def new_game():
     return json_response
 
 
-# @app.post("api/score-word")
-# def score_word():
+@app.post("api/score-word")
+def score_word():
+    response = request.json
+    game_id = response.get("gameId")
+    word = response.get("word")
+    game = games.get(game_id, "Game not found")
     
+    
+    # if not a word
+    # return {result: "not-word"}
+
+    if game.is_word_in_word_list(word) == False:
+        return jsonify({"result": "not-word"})
+
+    # if not on board
+    # return {result: "not-on-board"}
+
+    elif game.check_word_on_board(word) == False:
+        return jsonify({"result": "not-on-board"})
+
+    # if valid word 
+    # return {result: "ok"}
+    else:
+        score = games.play_and_score_word(word)
+        return jsonify({"result": "ok"})
+
+    
+
+
+
